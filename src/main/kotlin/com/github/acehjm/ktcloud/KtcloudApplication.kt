f9cb1f253ff21bf12c001cad75c011046fd1eb88
ameth <majhdkl@gmail.com>
2019-08-14 01:05:55 +0800

    init project

---
+++ src/main/kotlin/com/github/acehjm/ktcloud/KtcloudApplication.kt
@@ -0,0 +1,11 @@
+package com.github.acehjm.ktcloud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtcloudApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtcloudApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/acehjm/ktcloud/KtcloudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.acehjm.ktcloud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtcloudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
