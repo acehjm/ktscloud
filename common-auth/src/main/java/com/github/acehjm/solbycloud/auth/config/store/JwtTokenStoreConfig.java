d942b8a077d3a0b160ec1f942677e13bd0d9040d
ameth <majhdkl@gmail.com>
2019-12-18 23:16:55 +0800

    add auth jks

--- common-auth/src/main/java/com/github/acehjm/solbycloud/auth/config/store/JwtTokenStoreConfig.java
+++ common-auth/src/main/java/com/github/acehjm/solbycloud/auth/config/store/JwtTokenStoreConfig.java
@@ -2,9 +2,11 @@ package com.github.acehjm.solbycloud.auth.config.store;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 /**
  * com.github.acehjm.solbycloud.auth.config
@@ -22,9 +24,9 @@ public class JwtTokenStoreConfig {
 
     @Bean
     protected JwtAccessTokenConverter jwtAccessTokenConverter() {
-//        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "mySecretKey".toCharArray());
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("onejwt.jks"), "onejwt".toCharArray());
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("onejwt"));
         return converter;
     }
 
