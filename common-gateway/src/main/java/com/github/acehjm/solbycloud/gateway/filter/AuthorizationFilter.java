7f3b39d587a26bea794aa917f3fa40987062fda4
ameth <majhdkl@gmail.com>
2020-01-14 23:04:58 +0800

    add loadbalancer

--- common-gateway/src/main/java/com/github/acehjm/solbycloud/gateway/filter/AuthorizationFilter.java
+++ common-gateway/src/main/java/com/github/acehjm/solbycloud/gateway/filter/AuthorizationFilter.java
@@ -1,6 +1,7 @@
 package com.github.acehjm.solbycloud.gateway.filter;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +35,9 @@ public class AuthorizationFilter {
     private static final String Authorization = "Authorization";
     private static final String Bearer = "Bearer ";
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Bean
     @Order(0) // 过滤器的执行顺序::数字越大表示优先级越低，越后执行
     public GlobalFilter authGlobalFilter() {
@@ -49,8 +53,8 @@ public class AuthorizationFilter {
                 return handlerError(exchange, errorMsg);
             }
 
-            ResponseEntity<Object> entity = new RestTemplate()
-                    .getForEntity("http://localhost:8902/oauth/check_token?token=" + tokenSplit[1], Object.class);
+            String url = String.format("http://%s/oauth/check_token?token=", "common-auth");
+            ResponseEntity<Object> entity = restTemplate.getForEntity(url + tokenSplit[1], Object.class);
 
             return chain.filter(exchange);
         };
